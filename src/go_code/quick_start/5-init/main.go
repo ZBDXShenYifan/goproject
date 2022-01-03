2d5c74e550b665912b3c6e3cfe6156d34fc5dfa3
zbdxsyf <zbdxsyf@qq.com>
2022-01-03 17:00:54 +0800

    add 5-init folder

---
+++ src/go_code/quick_start/5-init/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "go_code/quick_start/5-init/lib1"
+	//mylib2 "go_code/quick_start/5-init/lib2"
+	. "go_code/quick_start/5-init/lib2"
+)
+
+func main() {
+	//lib1.Lib1Test()
+	//lib2.Lib2Test()
+	Lib2Test()
+}
